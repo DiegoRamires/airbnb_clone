1f36faa39ccc649de3b9f63575664ff101b25b4c
Diego Ramires <goncalves.ramires@gmail.com>
2018-01-26 13:40:41 -0200

    Prevent other users to access profile unless yours

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    redirect_to root_path, alert: "You don't have permission to see this profile" unless current_user == @user
   end
 end
